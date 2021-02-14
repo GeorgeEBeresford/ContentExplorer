89efe7c542ad862c0f0e714fcb1c9188780d6ba6
George Beresford <george.e.beresford@gmail.com>
2021-02-14 13:30:23 +0000

    Structure changes
    Moving Images/Styling/Scripts/Fonts to the "/Core" directory to keep the front-end code separate from the back-end code.

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -15,37 +15,33 @@ namespace ContentExplorer
         private static void RegisterStyles(BundleCollection bundles)
         {
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                "~/Content/bootstrap.css",
-                "~/Content/site.min.css"));
+                "~/Core/Styles/site.min.css"));
         }
 
         private static void RegisterScripts(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                "~/Scripts/Common/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                "~/Scripts/Common/jquery.validate*"));
+                "~/Core/Scripts/Common/jquery-{version}.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                "~/Scripts/Common/modernizr-*"));
+                "~/Core/Scripts/Common/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                "~/Scripts/Common/bootstrap.js"));
+                "~/Core/Scripts/Common/bootstrap.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/image/view").Include(
-                "~/Scripts/Image/View.js"));
+                "~/Core/Scripts/Image/View.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/image/index").Include(
-                "~/Scripts/Image/Index.js"));
+                "~/Core/Scripts/Image/Index.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/video/view").Include(
-                "~/Scripts/Video/View.js"));
+                "~/Core/Scripts/Video/View.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/video/index").Include(
-                "~/Scripts/Video/Index.js"));
+                "~/Core/Scripts/Video/Index.js"));
         }
     }
 }
